8946577440fdf1349e590745f3e8ee515c5da649
dxc <2318279444@qq.com>
2023-08-31 18:45:24 +0800

    第一次提交

---
+++ TestUtil/src/main/java/com/dc/testutil/MyUtils.java
@@ -0,0 +1,10 @@
+package com.dc.testutil;
+
+import android.util.Log;
+
+public class MyUtils {
+    public static void doSomething() {
+        // 实现你的工具方法
+        Log.e("aaa","我是自己写的一个工具类");
+    }
+}
