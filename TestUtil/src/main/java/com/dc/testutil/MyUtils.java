0fcf747d3bb59bfd296d61fd4bb6c17f5edd69b2
dxc <2318279444@qq.com>
2023-08-31 19:00:44 +0800

    222

--- TestUtil/src/main/java/com/dc/testutil/MyUtils.java
+++ TestUtil/src/main/java/com/dc/testutil/MyUtils.java
@@ -3,8 +3,13 @@ package com.dc.testutil;
 import android.util.Log;
 
 public class MyUtils {
-    public static void doSomething() {
+    public  void doSomething() {
         // 实现你的工具方法
         Log.e("aaa","我是自己写的一个工具类");
+    }
+
+    public  void doSomething2() {
+        // 实现你的工具方法
+        Log.e("aaa","我是自己写的一个工具类22222222222");
     }
 }
